980544c5ee4844498739863a1499e11ebc8cf0cd
HuangYongliang <master@hylstudio.cn>
2021-10-03 00:44:32 +0800

    copy correct template for gen test

---
+++ src/main/java/cn/hylstudio/mdse/demo/realworld/model/request/user/UserLoginRequestPayload.java
@@ -0,0 +1,20 @@
+//package cn.hylstudio.mdse.demo.realworld.model.request.user;
+//
+//public class UserLoginRequestPayload {
+//    private UserLoginRequestDto user;
+//
+//    public UserLoginRequestPayload() {
+//    }
+//
+//    public UserLoginRequestPayload(UserLoginRequestDto user) {
+//        this.user = user;
+//    }
+//
+//    public UserLoginRequestDto getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(UserLoginRequestDto user) {
+//        this.user = user;
+//    }
+//}
---
+++ src/main/java/cn/hylstudio/mdse/demo/realworld/model/response/user/UserLoginResponsePayload.java
@@ -0,0 +1,13 @@
+//package cn.hylstudio.mdse.demo.realworld.model.response.user;
+//
+//public class UserLoginResponsePayload {
+//    private UserLoginResponseDto user;
+//
+//    public UserLoginResponseDto getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(UserLoginResponseDto user) {
+//        this.user = user;
+//    }
+//}
